c1bc0d0e83384449b72e6ed8b3e2c98e7a52c00a
Emmanuel Hesry <ehesry@gmail.com>
2026-07-07 23:00:02 +0200

    Rename controllers

--- src/main/java/com/chatop/api/controller/HealthController.java
+++ src/main/java/com/chatop/api/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.chatop.api.controllers;
+package com.chatop.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/chatop/api/controller/RootController.java
+++ src/main/java/com/chatop/api/controller/RootController.java
@@ -1,4 +1,4 @@
-package com.chatop.api.controllers;
+package com.chatop.api.controller;
 
 import java.util.Map;
 
