d259c783ade12e43c0c8f53aba44aa2f863fbddb
Emmanuel Hesry <ehesry@gmail.com>
2026-07-11 16:30:02 +0200

    Authorize JWT token input in Swagger UI

--- src/main/java/com/chatop/api/config/OpenApiConfig.java
+++ src/main/java/com/chatop/api/config/OpenApiConfig.java
@@ -1,12 +1,17 @@
 package com.chatop.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String JWT_SECURITY_SCHEME = "bearerAuth";
 
     @Bean
     OpenAPI chatopOpenAPI() {
@@ -14,6 +19,13 @@ public class OpenApiConfig {
             .info(new Info()
                 .title("Châtop API")
                 .description("Backend REST API for the Châtop project.")
-                .version("v1"));
+                .version("v1"))
+            .addSecurityItem(new SecurityRequirement().addList(JWT_SECURITY_SCHEME))
+            .components(new Components()
+                .addSecuritySchemes(JWT_SECURITY_SCHEME, new SecurityScheme()
+                    .name(JWT_SECURITY_SCHEME)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
     }
 }
